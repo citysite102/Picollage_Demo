f6c5d0e6091edf2cd74a49da68c7e47b18201f5a
Samuel <citysite1025@gmail.com>
2018-10-06 23:29:50 +0800

    Initial Commit

---
+++ Picollage Demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Picollage Demo
+//
+//  Created by Samuel on 2018/10/6.
+//  Copyright © 2018 Samuel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
